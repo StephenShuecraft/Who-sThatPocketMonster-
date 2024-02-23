655987bd1f4cddfe1ecf5b639fb2e2e16a562ed9
StephenShuecraft <stephenshuecraft@gmail.com>
2024-02-22 22:12:07 -0600

    I made it all work

--- app/src/main/java/com/example/whosthatpocketmonster/MainActivity.kt
+++ app/src/main/java/com/example/whosthatpocketmonster/MainActivity.kt
@@ -7,6 +7,8 @@ import android.widget.ImageView
 import android.widget.RadioButton
 import kotlin.random.Random
 import android.content.Context
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 var score = 0
@@ -15,30 +17,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val pokName = arrayOf("Ekans", "Golbat", "Psyduck", "Sandshrew", "nothing" )
-        val pokimage = arrayOf("ekanssilsil", "golbatsil", "psyducksil", "sandshrewsil")
-
-        val randomNum = Random.nextInt(0, 4)
-
         val rad1 =  findViewById(R.id.radioButton) as RadioButton
         val rad2 =  findViewById(R.id.radioButton2) as RadioButton
         val rad3 =  findViewById(R.id.radioButton3) as RadioButton
         val rad4 =  findViewById(R.id.radioButton4) as RadioButton
 
         val imag1 = findViewById<ImageView>(R.id.imageView)
+        val imag2 = findViewById<ImageView>(R.id.imageView2)
+
+        val scoreText = findViewById<TextView>(R.id.score)
 
         val but1 = findViewById<Button>(R.id.button)
+        val but2 = findViewById<Button>(R.id.button2)
+
+
+
+    fun theWholeThing(){
+        val pokName = arrayOf("Ekans", "Golbat", "Psyduck", "Sandshrew", "nothing" )
+
+        val randomNum = Random.nextInt(0, 4)
+
 
         var answer = 0
         if (randomNum == 0){
             imag1.setImageResource(R.drawable.ekanssil)
+            imag2.setImageResource(R.drawable.ekans)
         }else if (randomNum == 1){
             imag1.setImageResource(R.drawable.golbatsil)
+            imag2.setImageResource(R.drawable.golbat)
         }else if (randomNum == 2){
             imag1.setImageResource(R.drawable.psyducksil)
+            imag2.setImageResource(R.drawable.psyduck)
         }else{
             imag1.setImageResource(R.drawable.sandshrewsil)
+            imag2.setImageResource(R.drawable.sandshrew)
         }
 
         rad1.setText(pokName[0])
@@ -67,6 +79,7 @@ class MainActivity : AppCompatActivity() {
             rad1.isChecked = false
         }
         but1.setOnClickListener {
+            imag1.visibility = View.INVISIBLE
             if(rad1.isChecked){
             answer = 0
             }else if (rad2.isChecked){
@@ -80,22 +93,32 @@ class MainActivity : AppCompatActivity() {
             }
             if (pokName[answer] == pokName[randomNum]) {
                 score += 1
-                val yourScore: Int = score// ... obtain your score
-
-                val message = "Your score is: $yourScore"
+                scoreText.setText("score = $score")
+            } else {
+                val message = "You suck!!!!!!"
                 val duration = Toast.LENGTH_SHORT
 
                 Toast.makeText(this, message, duration).show()
-            } else {
-                false
             }
 
+            but1.visibility = View.INVISIBLE
+            but2.visibility = View.VISIBLE
+            rad1.isChecked = false
+            rad2.isChecked = false
+            rad3.isChecked = false
+            rad4.isChecked = false
 
-
-
+        }}
+        but2.setOnClickListener{
+            but1.visibility = View.VISIBLE
+            but2.visibility = View.INVISIBLE
+            imag1.visibility = View.VISIBLE
+            theWholeThing()
 
 
         }
+        theWholeThing()
+
 
     }
 }
